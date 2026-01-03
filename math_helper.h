aaa679eb1caaf0b20ecf5559843740a264e2df25
Jay <123280045+WDDETER@users.noreply.github.com>
2026-01-02 22:14:28 -0500

    Added OBJ Loading, Fixed Typo, Back-face Culling.

---
+++ math_helper.h
@@ -0,0 +1,17 @@
+//
+//      math_helper.h  (12/30/2025)
+//      by Jaidev Bormann
+//
+
+
+#ifndef _OHM_MATH_HELPER_H
+#define _OHM_MATH_HELPER_H
+
+
+#define PI 3.14159f
+
+#define DEG_TO_RAD(x) (x * (PI / 180.0f))
+#define RAD_TO_DEG(x) (x * (180.0f / PI))
+
+
+#endif
